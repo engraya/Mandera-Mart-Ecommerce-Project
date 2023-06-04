620446124c88e6cab5091726982974d60d02282d
Oref2001 <131554994+Oref2001@users.noreply.github.com>
2023-06-04 13:15:54 +0100

    Update settings.py

--- WebCanteen/settings.py
+++ WebCanteen/settings.py
@@ -82,8 +82,6 @@ WSGI_APPLICATION = 'WebCanteen.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mandera_db',
         'USER': 'favourboluwatife',
