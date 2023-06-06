81029aecc91ae28e63945f0f787305bfe3a86b4e
EngrAhmad <ahmadyakubuaya@gmail.com>
2023-06-06 05:36:49 +0100

    End Commit

--- WebCanteen/wsgi.py
+++ WebCanteen/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'WebCanteen.settings.dev'
+DJANGO_SETTINGS_MODULE="WebCanteen.settings.dev"
 
 application = get_wsgi_application()
